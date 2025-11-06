13d272a4c5aa44ad97decd2b2d5a8ef7c2a45e5b
Mahesh Vannavada <139055724+maheshvannavada@users.noreply.github.com>
2025-11-06 22:32:33 +0100

    Add query for patients in 'Surgery' with low satisfaction
    Add SQL query to find patients in 'Surgery' with low satisfaction scores.

--- Day_2_challenge.sql
+++ Day_2_challenge.sql
@@ -1,3 +1,5 @@
+#Question:-Find all patients admitted to 'Surgery' service with a satisfaction score below 70, showing their patient_id, name, age, and satisfaction score.
+
 use hospital;
 select patient_id, name, age, satisfaction as satisfaction_score from patients
 where service = 'Surgery' and satisfaction <70;
