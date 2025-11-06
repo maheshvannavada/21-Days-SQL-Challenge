2f838c9e69d1998083367083824a76702904da2a
Mahesh Vannavada <139055724+maheshvannavada@users.noreply.github.com>
2025-11-06 23:11:18 +0100

    Update Day_3_challenge.sql

--- Day_3_challenge.sql
+++ Day_3_challenge.sql
@@ -1,3 +1,6 @@
+#Question: Retrieve the top 5 weeks with the highest patient refusals across all services, 
+#showing week, service, patients_refused, and patients_request. Sort by patients_refused in descending order.
+
 use hospital;
 
 SELECT week, service, sum(patients_refused) as patients_refused, sum(patients_request) as patients_request
