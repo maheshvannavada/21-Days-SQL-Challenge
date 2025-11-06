d7c99a553c56aa1c17ce1835c2293a5785a2fb5b
Mahesh Vannavada <139055724+maheshvannavada@users.noreply.github.com>
2025-11-06 23:12:04 +0100

    Clarify SQL comment for patient satisfaction query
    Modified the SQL comment to clarify the query's intent.

--- Day_4_challenge.sql
+++ Day_4_challenge.sql
@@ -1,5 +1,5 @@
 #Question: Find the 3rd to 7th highest patient satisfaction scores from the patients table, showing patient_id, 
-  name, service, and satisfaction. Display only these 5 records.
+#name, service, and satisfaction. Display only these 5 records.
 
 use hospital;
 
